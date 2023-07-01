e528a0d3b34eafccf73dc3b000075f2bb2b3fa0b
RgerWeb <mckeenweb@gmail.com>
2023-07-01 16:05:54 +0800

    plotting own graph

---
+++ spyder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 20:44:06 2023
+
+@author: Rger
+"""
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import pprint
+
+from Historic_Crypto import Cryptocurrencies
+from Historic_Crypto import HistoricalData
+import yfinance as yf
+
+pip install --upgrade spyder
